f6c4a2da87d7c8bf2f47bd2d15126e200c724cbc
FreemanTheMaverick <freemanz771746166@163.com>
2021-08-12 01:01:04 +0800

    MBPT not applicable yet

--- main.cpp
+++ main.cpp
@@ -1,28 +1,38 @@
 #include "HartreeFock.h"
+#include "MBPT.h"
 #include <iostream>
 #include <libint2.hpp>
 
 
 int main(){
 	initialize();
-	HartreeFockJob job;
-	job.setXYZ("f.xyz");
-	job.setBasisSet("sto-3g");
-	job.setGuess("123");
-	job.Compute();
-	finalize();
-	std::cout<<"HF energy: "<<job.Energy<<std::endl;
+	HartreeFockJob hfjob;
+	string filename,basisname;
+	std::cin>>filename>>basisname;
+	hfjob.setXYZ(filename);
+	hfjob.setBasisSet(basisname);
+	hfjob.setGuess("hcore");
+	hfjob.Compute();
+	std::cout<<"HF energy: "<<hfjob.Energy<<std::endl;
 	std::cout<<"Orbital energies:"<<std::endl;
-	std::cout<<job.OrbitalEnergies<<std::endl;
+	std::cout<<hfjob.OrbitalEnergies<<std::endl;
 	std::cout<<"Coefficient matrix:"<<std::endl;
-	std::cout<<job.CoefficientMatrix<<std::endl;
+	std::cout<<hfjob.CoefficientMatrix<<std::endl;
 	std::cout<<"Density matrix:"<<std::endl;
-	std::cout<<job.DensityMatrix<<std::endl;
+	std::cout<<hfjob.DensityMatrix<<std::endl;
 	std::cout<<"J matrix:"<<std::endl;
-	std::cout<<job.JMatrix<<std::endl;
+	std::cout<<hfjob.JMatrix<<std::endl;
 	std::cout<<"K matrix:"<<std::endl;
-	std::cout<<job.KMatrix<<std::endl;
-
+	std::cout<<hfjob.KMatrix<<std::endl;
+	
+	MP2Job mp2job;
+	mp2job.setXYZ(filename);
+	mp2job.setBasisSet(basisname);
+	mp2job.setCoefficientMatrix(DuplicateCol(hfjob.CoefficientMatrix));
+	mp2job.setOrbitalEnergies(DuplicateRow(hfjob.OrbitalEnergies));
+	mp2job.Compute();
+	finalize();
+	std::cout<<"MP2 correlation energy: "<<mp2job.CorrelationEnergy<<std::endl;
 }
 
 	
