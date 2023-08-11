35e25e97352b5ce8fb6005040231765cc63e50ff
FreemanTheMaverick <63384635+FreemanTheMaverick@users.noreply.github.com>
2023-08-11 18:36:09 +0800

    OSQP.c -> OSQP.cpp

--- OSQP.h
+++ OSQP.h
@@ -1,3 +1,1 @@
-extern "C"{
-	void QuadraticProgramming(int size,double * helements,int * hrowindeces,int * hcolpointers,double * g,double * aelements,int * arowindeces,int * acolpointers,int nconstraints,double * l,double * u,double * x);
-}
+void QuadraticProgramming(int size,double * helements,int * hrowindeces,int * hcolpointers,double * g,double * aelements,int * arowindeces,int * acolpointers,int nconstraints,double * l,double * u,double * x);
